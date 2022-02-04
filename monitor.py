cbdc85b09ee82f93df36652f77573d90214525d3
gcommit <mariusgebert@gmx.de>
2022-02-04 15:45:07 +0100

    added example bandwith csv

--- monitor.py
+++ monitor.py
@@ -5,14 +5,14 @@ import csv
 s = speedtest.Speedtest()
 
 with open('bandwidthdata.csv', mode='w') as speedcsv:
-    csv_writer = csv.DictWriter(speedcsv, fieldnames=['downspeed', 'upspeed'])
+    csv_writer = csv.DictWriter(speedcsv, fieldnames=['download', 'upload'])
     csv_writer.writeheader()
     while True:
-        downspeed = round((round(s.download()) / 1048576), 2)
-        upspeed = round((round(s.upload()) / 1048576), 2)
+        download = round((round(s.download()) / 1048576), 2)
+        upload = round((round(s.upload()) / 1048576), 2)
         csv_writer.writerow({
-            'downspeed': downspeed,
-            "upspeed": upspeed
+            'download': download,
+            "upload": upload
         })
         speedcsv.flush()
         #time.sleep(30)
