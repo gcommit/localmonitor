9d46e5bb11b73f43854639cfb68be9ea1a386791
gcommit <mariusgebert@gmx.de>
2022-02-07 12:35:48 +0100

    removed unusefull code

--- graph.py
+++ graph.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import csv
-import matplotlib.ticker as ticker
 import pandas as pandas
 import numpy as np
 
@@ -10,9 +8,11 @@ times = data["time"]
 download = data["download"]
 upload = data["upload"]
 
-plt.figure(figsize=(10, 10))
+plt.style.use('ggplot')
+plt.figure(figsize=(20, 10))
 plt.plot(data.time, data.download, label='download', color='r')
 plt.plot(data.time, data.upload, label='upload', color='b')
+plt.gcf().autofmt_xdate()
 plt.xlabel('time')
 plt.ylabel('speed in Mb/s')
 plt.title("internet speed")
@@ -20,5 +20,5 @@ plt.legend()
 
 # SAVE OR SHOW THE GRAPH
 
-#plt.savefig('networkgraph.jpg', bbox_inches='tight')
-plt.show()
+plt.savefig('networkgraph.jpg', bbox_inches='tight')
+#plt.show()
